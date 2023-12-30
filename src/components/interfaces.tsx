109b761d2b53b771b7cd48d5533070a4a0814dad
Travis <16505078+TravJav@users.noreply.github.com>
2023-12-29 23:58:25 -0500

    interface for the contact information in context of a new referral

---
+++ src/components/interfaces.tsx
@@ -0,0 +1,12 @@
+export interface IContactInformation {
+  name: string;
+  first: string;
+  last: string;
+  phoneNumber: string;
+  contactLanguage: string;
+  address: string;
+  positiveQuote: string;
+  indexColour: string;
+  notesReason: string;
+  email: string;
+}
---
+++ src/components/utils/names_constants.tsx
@@ -0,0 +1,56 @@
+
+  const existingReferrals =[
+    {
+        "name": "Rosa Diaz",
+        "first": "Rosa",
+        "last": "Diaz",
+        "phoneNumber": "5058929273",
+        "contactLanguage": "Spanish",
+        "address": "1234 Some Street, City, Country",
+        "positiveQuote": "Amazing Referral!",
+        "indexColour": "#25A575",
+        "notesReason": "ed ut perspiciatis unde omnis iste natus error sit volupta",
+        "email": "example@email.com"
+    },
+    {
+        "name": "Amy Santiago",
+        "first": "Amy",
+        "last": "Santiago",
+        "phoneNumber": "8877541103",
+        "contactLanguage": "Spanish",
+        "address": "1234 Some Street, City, Country",
+        "positiveQuote": "Amazing Referral!",
+        "indexColour": "#2595A5",
+        "notesReason": " dolorem ipsum quia dolor sit amet, consectetur, adipisci v",
+        "email": "testuser123@email.com"
+    },
+    {
+        "name": "Raymond Holt",
+        "first": "Raymon",
+        "last": "Holt",
+        "phoneNumber": "4479305485",
+        "contactLanguage": "German",
+        "address": "1234 Some Street, City, Country",
+        "positiveQuote": "Amazing Referral!",
+        "indexColour": "#3A719B",
+        "notesReason": "magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis ",
+        "email": "example@email.com"
+    },
+    {
+        "name": "Terence Jeffords",
+        "first": "Terence",
+        "last": "Jeffords",
+        "phoneNumber": "1340700029",
+        "contactLanguage": "German",
+        "address": "1234 Some Street, City, Country",
+        "positiveQuote": "Amazing Referral!",
+        "indexColour": "#254B7A",
+        "notesReason": "dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+        "email": "testuser123@email.com"
+
+    }
+]
+
+
+
+export default existingReferrals;
