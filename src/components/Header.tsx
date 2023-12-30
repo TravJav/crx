b9177b3eeffa4e64d5e979c1f09b2f87f5e35f19
Travis <16505078+TravJav@users.noreply.github.com>
2023-12-29 23:57:27 -0500

    header component implemented in parent

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { Typography, Box } from '@mui/material';
+
+const Header = () => {
+  const headerStyles = {
+    fontFamily: 'Montserrat',
+    fontSize: '30px',
+    fontWeight: 500,
+    lineHeight: '85px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    color: '0B2B5B',
+    backgroundColor: '#FFFFFF',
+  };
+
+  return (
+    <Box py={0}>
+      <Typography variant="h2" sx={headerStyles}>
+        Patient Referral Form
+      </Typography>
+      <Typography variant="h2" sx={headerStyles}>
+        ConcussionRx
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
